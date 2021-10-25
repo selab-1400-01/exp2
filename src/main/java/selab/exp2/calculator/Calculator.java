067532b9d90840ec9c3e4b9e6c1339ed9496eb01
momt99 <mohammad.omt99@yahoo.com>
2021-10-25 13:38:04 +0330

    Add calculator interface as well as test implementation

---
+++ src/main/java/selab/exp2/calculator/Calculator.java
@@ -0,0 +1,16 @@
+package selab.exp2.calculator;
+
+public class Calculator {
+    private double currentValue;
+
+    public double getCurrentValue() {
+        return currentValue;
+    }
+
+    public void setCurrentValue(double currentValue) {
+        this.currentValue = currentValue;
+    }
+
+    public void clickButton(String name) {
+    }
+}
--- src/test/java/selab/exp2/calculator/Stepdefs.java
+++ src/test/java/selab/exp2/calculator/Stepdefs.java
@@ -4,25 +4,28 @@ import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 public class Stepdefs {
     private Calculator calculator;
 
     @Before
     public void setup() {
-
+        this.calculator = new Calculator();
     }
 
     @Given("Input value of {double}")
     public void inputValueOf(double value) {
+        this.calculator.setCurrentValue(value);
     }
 
     @When("I press button {string}")
     public void iPressButtonSqr(String button) {
+        this.calculator.clickButton(button);
     }
 
     @Then("I expect the result {double}")
     public void iExpectTheResult(double result) {
-
+        Assert.assertEquals(result, this.calculator.getCurrentValue(), 1e-18);
     }
 }
